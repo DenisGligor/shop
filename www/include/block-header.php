a1b040f76e782443bf48b0344536fba14fcdfe0a
Denis Gligor <deni.gligor94@gmail.com>
2015-10-19 23:10:36 +0300

    new commit file index0.php

---
+++ www/include/block-header.php
@@ -0,0 +1,33 @@
+<div id="block-header">
+<div id="header-top-block">
+
+<ul id="header-top-menu">
+
+<li>Ваше місто - <span>Київ</span></li>
+<li><a href="o-nas.php">Про нас</a></li>
+<li><a href="magaziny.php">Магазини</a></li>
+<li><a href="contacts.php">Контакти</a></li>
+</ul>
+
+
+<p id="reg-auth-title"><a class="top-auth">Вхід</a><a href="registration.php">Регістрація</a></p>
+
+
+</div>
+<div id="top-line"></div>
+
+<img id="img-logo" src="/images/logo.png"/>
+
+<div id="personal-info">
+<h3 align="right">+380501488324</h3>
+
+<img src="/images/phone-icon.png" />
+<p align="right">Режим роботи</p>
+<p align="right">Будні дні: з 9:00 до 20:00</p>
+<p align="right">Субота, Неділя - вихідні</p>
+
+<img id="img-logo" src="/images/time-icon.png"/>
+
+
+</div>
+</div>
